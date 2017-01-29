d6fe1a5fa8bb5904afdbac048e4bc4fc0d446996
Akshay088 <akshay088@live.com>
2017-01-29 23:43:12 +0400

    // Web App

--- app.R
+++ app.R
@@ -1,26 +1,73 @@
-library(shiny)
+if(!isNamespaceLoaded("shinydashboard")) library(shinydashboard)
+if(!isNamespaceLoaded("shiny")) library(shiny)
 
-ui <- fluidPage(
+### UI
+
+ui <- dashboardPage(
   
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file1", "Choose CSV File",
-                accept = c(
-                  "text/csv",
-                  "text/comma-separated-values,text/plain",
-                  ".csv")
+  dashboardHeader(title = "Yelp"),
+  
+  dashboardSidebar(
+    
+    sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "CSV", icon = icon("th"))
+  )
+  
+  ),
+  
+  dashboardBody(
+    
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
       ),
-      tags$hr(),
-      checkboxInput("header", "Header", TRUE)
-    ),
-    mainPanel(
-      tableOutput("contents")
+      
+      # Second tab content
+      tabItem(tabName = "CSV",
+              h2("Widgets tab content"),
+              sidebarLayout(
+                sidebarPanel(
+                  fileInput("file1", "Choose CSV File",
+                            accept = c(
+                              "text/csv",
+                              "text/comma-separated-values,text/plain",
+                              ".csv")
+                  ),
+                  tags$hr(),
+                  checkboxInput("header", "Header", TRUE)
+                ),
+                mainPanel(
+                  tableOutput("contents")
+                )
+              )
+          )
+      
     )
+    
   )
   
 )
 
-server <- function(input, output){
+
+### Server Side
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
   
   output$contents <- renderTable({
     # input$file1 will be NULL initially. After the user selects
@@ -36,8 +83,7 @@ server <- function(input, output){
     read.csv(inFile$datapath, header = input$header)
     # View(inFile)
   })
-  
-  
 }
+
 
 shinyApp(ui = ui, server = server)

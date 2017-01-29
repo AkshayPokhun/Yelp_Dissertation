fde39f5a3bd02253ac97722c31beac4d3e6c2919
Akshay088 <akshay088@live.com>
2017-01-29 22:00:12 +0400

    // Build Three

--- Microsoft Entity Linking.R
+++ Microsoft Entity Linking.R
@@ -6,9 +6,9 @@ library(httr)
 
 # Converting the Request body(Dataframe) to Request body(JSON)
 
-  getEntity <- function(text) {
-    
-    url <- "https://westus.api.cognitive.microsoft.com/entitylinking/v1.0/link"
+getEntity <- function(text) {
+  
+  url <- "https://westus.api.cognitive.microsoft.com/entitylinking/v1.0/link"
     key <- "d21e5cfe59a646079793f840814dd0e2"
     
     result <- POST(url,
